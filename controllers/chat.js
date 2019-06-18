a5f55fcbcf9a87e677c7786a657947a6cc050636
msiavashi <mohammad.siav@hotmail.com>
2019-06-19 03:32:21 +0430

    some controller added

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,7 +1,75 @@
+var messages = [];
+var userName;
+
+
+
 exports.initial = (req, res, next) => {
-    
+    const data = {
+        challenge: req.query.challenge,
+        data: {
+            type: "text",
+            elements: [], 
+            text: "Hi, What is your name?"
+        }
+    }
+    return res.json(data);
 }
 
-exports.getNextBirthday = (req, res, next) => {
+function onNameReceived(req, res, next){
+    let splited = req.query.body.answer.text.split(" ");
+    name = splited[splited.length - 1];
+    const response = {
+        type: "text",
+        elements: [], 
+        text: "Thanks " + name + " would you know your next birthday?"
+    }
+    return res.json(response);
+}
 
+function onAccept(req, res, next){
+    const response = {
+        type: "text",
+        elements: [],
+        text: "please enter your birthday (YYYY-MM-DD)"
+    }
+    return res.json(response);
+}
+
+function isValidDate(dateString) {
+  var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if(!dateString.match(regEx)) return false;  // Invalid format
+  var d = new Date(dateString);
+  var dNum = d.getTime();
+  if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+  return d.toISOString().slice(0,10) === dateString;
+}
+
+exports.handleResponse = (req, res, next) => {
+    const data = req.query.body;
+    if(data.text.startsWith("my name is")){
+        onNameReceived(data);
+    }else if(data.text.toLowerCase() == "yes" || data.text.toLowerCase() == "yep"){
+        onAccept(req, res, next);
+    }else if(data.text.toLowerCase() == "no" || data.text.toLowerCase() == "nah"){
+        return res.send({
+            type: "text",
+            elements: [], 
+            text: "GoodBye !!!"
+        });
+    }else if (isValidDate(data.text)){
+        getNextBirthday(req, res, next);
+    }
+}
+
+function getNextBirthday(req, res, next) {
+    var birthday = req.query.body.text.split("-");
+    var today = new Date();
+    var next = new Date(today.getFullYear(), parseInt(birthday[1])-1, parseInt(birthday[2]));
+    if(today>next) next.setFullYear(y+1);
+    const repsonse = {
+        type: "text",
+        elements: [],
+        text: Math.round((next-today)/8.64e7)
+    }
+    return res.json(response);
 }
--- routes/API/chat.js
+++ routes/API/chat.js
@@ -3,41 +3,20 @@ var router = express.Router();
 var chatController = require('../../controllers/chat');
 
 const token = 'TestToken';  //verification token for webhook
-var userAnswers = [];
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
     if (req.query.token !== token){
         return res.sendStatus(401);
     }
-    const data = {
-        challenge: req.query.challenge,
-        data: {
-            type: "text",
-            elements: {},
-            text: "Hi, What is your name?"
-        }
-    }
-    return res.json(data);
+    chatController.initial(req, res, next);
 });
 
 router.post('/', function(req, res, next){
      if (req.query.token !== token) {
         return res.sendStatus(401);
     }
- 
-    console.log(req.body);
-
-    const data = [
-        {
-            type: 'text',
-            elements: [
-                "hi",
-                "hello"
-            ]
-        }
-    ];
-    res.json(data);
+    chatController.handleResponse(req, res, next);
 });
 
 module.exports = router;
